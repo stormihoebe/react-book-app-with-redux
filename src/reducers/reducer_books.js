4bda66280062ff9c52441635e6648593082b49e3
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-09-04 14:38:55 -0700

    add additional book details in books_reducer and display them in book detail

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,7 +1,7 @@
 export default function() {
   return [
-    {title: 'Javascript'},
-    {title: 'Harry Potter'},
-    {title: 'The Dark Tower'},
+    {title: 'Javascript', pages: 492},
+    {title: 'Harry Potter', pages: 600},
+    {title: 'The Dark Tower', pages: 5}
   ]
 }
